0f2f30abfb543ea5fe0fcaae0760e5620572a192
haesungJoo <haesung0728@gmail.com>
2022-07-24 18:43:31 +0900

    [solution] N으로 표현 시작

---
+++ src/main/kotlin/programmers/highscorekit/ExpressedByN.kt
@@ -0,0 +1,15 @@
+package programmers.highscorekit
+
+/**
+ * 설명
+ * - 프로그래머스 > 고득점 kit > 이분탐색 > level3 > N으로 표현
+ * - 경로 : https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=kotlin
+ *
+ * */
+
+class ExpressedByN {
+    fun solution(N: Int, number: Int): Int{
+        var answer = 0
+        return answer;
+    }
+}
