d1484b455a308c6c60e4f6e29d7932dfa9e13a0d
sggnology <sggnology@gmail.com>
2022-08-04 02:03:20 +0900

    [solution] `H-index` 문제풀이 끝

--- src/main/kotlin/programmers/highscorekit/HIndex.kt
+++ src/main/kotlin/programmers/highscorekit/HIndex.kt
@@ -6,4 +6,51 @@ package programmers.highscorekit
  * - 경로 : https://school.programmers.co.kr/learn/courses/30/lessons/42747
  * */
 class HIndex {
+    fun solution(citations: IntArray): Int{
+//        val sortedCitations = citations.sorted().toIntArray()
+//
+//        val sortedCitationsLength = citations.size
+//        var midIdx: Int = sortedCitationsLength/2
+//
+//        while((sortedCitationsLength - midIdx) < sortedCitations[midIdx]){
+//            if(midIdx == 0){
+//                break
+//            }
+//            midIdx -= 1
+//        }
+//
+//        println(sortedCitations[midIdx])
+//
+//        return sortedCitationsLength - midIdx
+
+//        val sortedList = citations.sorted()
+//
+//        val length = sortedList.size
+//        val midIdx = length/2
+//        val midValue = sortedList[midIdx]
+//
+//        if(length < midIdx){
+//            return length
+//        }
+//        else{
+//            for(possibleAnswer in midValue..1){
+//                if((length - midIdx))
+//            }
+//        }
+
+        val reverseSortedList = citations.sorted().reversed()
+        var index = 0
+
+        for(value in reverseSortedList){
+            if(value <= index){
+                break
+            }
+            index += 1
+        }
+
+        println(index)
+
+        return index
+
+    }
 }
