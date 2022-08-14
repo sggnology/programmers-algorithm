dda97819b65f3968ba87713e85c8b15ea9f548de
sggnology <sggnology@gmail.com>
2022-08-14 18:14:06 +0900

    [solution > test > 최대공약수최소공배수] 유클리드 법칙으로 풀어보기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,10 @@ import programmers.highscorekit.ExpressedByN
 import programmers.highscorekit.HIndex
 import programmers.highscorekit.TargetNumber
 import programmers.summerwinter.FineSquare
+import programmers.test.최대공약수최소공배수
 
 fun main(args: Array<String>) {
 
-    FineSquare().solution(8, 12)
+    최대공약수최소공배수().solution(3, 12)
 
 }
