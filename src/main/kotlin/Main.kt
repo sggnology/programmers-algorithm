3175e5ee732e7500640ce8dcabd036cbf4d693cf
sggnology <sggnology@gmail.com>
2022-08-16 01:11:42 +0900

    [solution] `거리두기 확인하기`문제 클래스 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 import programmers.highscorekit.ExpressedByN
 import programmers.highscorekit.HIndex
 import programmers.highscorekit.TargetNumber
+import programmers.kakao2021채용연계형인턴십.거리두기확인하기
 import programmers.summerwinter.FineSquare
 
 fun main(args: Array<String>) {
 
-    FineSquare().solution(8, 12)
+//    var params = arrayOf(arrayOf("POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"), arrayOf("POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"))
+    var params = arrayOf(arrayOf("POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"))
+
+    거리두기확인하기().solution(params)
 
 }
---
+++ src/main/kotlin/programmers/kakao2021채용연계형인턴십/거리두기확인하기.kt
@@ -0,0 +1,13 @@
+package programmers.kakao2021채용연계형인턴십
+
+class 거리두기확인하기 {
+    fun solution(places: Array<Array<String>>): IntArray{
+        var answer: IntArray = intArrayOf()
+
+        var testVar = arrayOf("POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP")
+
+
+
+        return answer
+    }
+}
