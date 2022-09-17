c75f4f2de9557b6a75f66f3b3bd55b3d495dbc04
sggnology <sggnology@gmail.com>
2022-09-17 19:49:28 +0900

    [solution > test > 최솟값] 풀이

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,13 @@ import programmers.highscorekit.HIndex
 import programmers.highscorekit.TargetNumber
 import programmers.summerwinter.FineSquare
 import programmers.test.최대공약수최소공배수
+import programmers.test.최솟값
 
 fun main(args: Array<String>) {
 
-    최대공약수최소공배수().solution(3, 12)
+    val a = intArrayOf(1,2)
+    val b = intArrayOf(3,4)
+
+    최솟값().solution(a, b);
 
 }
