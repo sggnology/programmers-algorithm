7071b1f4b44d7b0ee6383f2ab26a30d6383de3ca
sggnology <sggnology@gmail.com>
2022-10-24 12:16:48 +0900

    [연습문제(test) > 롤케이크자르기] 문제 풀이

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,33 @@
-import programmers.practice.CountryOf124
 import programmers.practice.최댓값과최솟값
+import programmers.test.롤케이크자르기
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 fun main(args: Array<String>) {
 
-//    최댓값과최솟값().solution("-1 -2 -3 -4")
-    최댓값과최솟값().solution("-1 -1")
+//    println(
+//        arrayOf(1,2,3,4).groupingBy { it }.eachCount()
+//    )
+//
+//    println(
+//        intArrayOf(1,2).toTypedArray().groupingBy { it }.eachCount()
+//    )
+
+//    롤케이크자르기().solution(intArrayOf(1,2,1,3,1,4,1,2))
+    롤케이크자르기().solution(intArrayOf(1,2,3,1,4))
+//    롤케이크자르기().solution(intArrayOf(1,2))
+
+//    var amplititudes  = (1..1000000).map { Random().nextInt() }
+//
+//    val pattern = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
+//
+//    println(LocalDateTime.now().format(pattern))
+//    for(i in 0..100){
+//        amplititudes.toSet()
+//
+//    }
+//    println(LocalDateTime.now().format(pattern))
+
 
 }
