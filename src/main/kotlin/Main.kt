e69ae8612bd31e2a5b575f23d864d4038e4ccd4c
sggnology <sggnology@gmail.com>
2023-01-01 19:55:48 +0900

    [test > 유사 칸토어 비트열] 문제 푸는중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,10 @@
 import programmers.highscorekit.단어변환
-import programmers.test.우박수열_정적분
-import programmers.test.점찍기
-import programmers.test.테이블_해시_함수
-import programmers.test.혼자놀기의달인
+import programmers.test.*
 import programmers.코딩테스트입문.외계행성의나이
 import java.lang.Exception
 
 fun main(args: Array<String>) {
 
-    //[[2,2,6],[1,5,10],[4,2,9],[3,8,3]]
-    val tableData = arrayOf(intArrayOf(2,2,6), intArrayOf(1,5,10), intArrayOf(4,2,9), intArrayOf(3,8,3))
-
-    테이블_해시_함수().solution(tableData,2,1,3)
+    유사_칸토어_비트열().solution(10,4,17)
 
 }
