eb0a0d8913e0ebeb7a16f1c1b2bc43b0a395c077
sggnology <sggnology@gmail.com>
2023-02-14 00:33:36 +0900

    [test > 뒤에있는큰수찾기] 제일 큰값을 배제함으로써 한문제 더 맞춤

--- src/main/kotlin/programmers/test/뒤에있는큰수찾기.kt
+++ src/main/kotlin/programmers/test/뒤에있는큰수찾기.kt
@@ -1,33 +1,41 @@
 package programmers.test
 
 class 뒤에있는큰수찾기 {
-    fun solution(numbers: IntArray): IntArray {
-
-    }
-
 //    fun solution(numbers: IntArray): IntArray {
 //
-//        val solution = IntArray(numbers.size){0}
-//        solution[solution.size - 1] = -1
-//
-//        for(outerNumberIndex in numbers.indices){
-//            val targetNumber = numbers[outerNumberIndex]
-//
-//            for(innerNumberIndex in outerNumberIndex + 1 until numbers.size){
-//                val compareNumber = numbers[innerNumberIndex]
-//                if(targetNumber < compareNumber){
-//                    solution[outerNumberIndex] = compareNumber
-//                    break
-//                }
-//
-//                if(innerNumberIndex == numbers.size - 1){
-//                    solution[outerNumberIndex] = -1
-//                }
-//            }
-//        }
-//
-////        println(solution.toList())
-//
-//        return solution
 //    }
+
+    fun solution(numbers: IntArray): IntArray {
+
+        val solution = IntArray(numbers.size){0}
+        solution[solution.size - 1] = -1
+
+        val maxNumberInNumbers = numbers.maxOf { it }
+
+        for(outerNumberIndex in numbers.indices){
+            val targetNumber = numbers[outerNumberIndex]
+
+            for(innerNumberIndex in outerNumberIndex + 1 until numbers.size){
+                val compareNumber = numbers[innerNumberIndex]
+
+                if(targetNumber == maxNumberInNumbers){
+                    solution[outerNumberIndex] = -1
+                    break
+                }
+
+                if(targetNumber < compareNumber){
+                    solution[outerNumberIndex] = compareNumber
+                    break
+                }
+
+                if(innerNumberIndex == numbers.size - 1){
+                    solution[outerNumberIndex] = -1
+                }
+            }
+        }
+
+//        println(solution.toList())
+
+        return solution
+    }
 }
