b1a15b663891c2184bdb8222486f18f13000e807
sggnology <sggnology@gmail.com>
2023-02-14 00:27:47 +0900

    [test > 뒤에있는큰수찾기] 문제풀이중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
-import programmers.highscorekit.단어변환
-import programmers.test.우박수열_정적분
-import programmers.test.점찍기
+import programmers.test.뒤에있는큰수찾기
 import programmers.test.혼자놀기의달인
-import programmers.코딩테스트입문.외계행성의나이
-import java.lang.Exception
 
 fun main(args: Array<String>) {
 
-    혼자놀기의달인().solution(intArrayOf(8,6,3,7,2,5,1,4))
+    val array1 = intArrayOf(2,3,3,5)
+    val array2 = intArrayOf(9,1,5,3,6,2)
+
+    뒤에있는큰수찾기().solution(array2)
 }
---
+++ src/main/kotlin/programmers/test/뒤에있는큰수찾기.kt
@@ -0,0 +1,33 @@
+package programmers.test
+
+class 뒤에있는큰수찾기 {
+    fun solution(numbers: IntArray): IntArray {
+
+    }
+
+//    fun solution(numbers: IntArray): IntArray {
+//
+//        val solution = IntArray(numbers.size){0}
+//        solution[solution.size - 1] = -1
+//
+//        for(outerNumberIndex in numbers.indices){
+//            val targetNumber = numbers[outerNumberIndex]
+//
+//            for(innerNumberIndex in outerNumberIndex + 1 until numbers.size){
+//                val compareNumber = numbers[innerNumberIndex]
+//                if(targetNumber < compareNumber){
+//                    solution[outerNumberIndex] = compareNumber
+//                    break
+//                }
+//
+//                if(innerNumberIndex == numbers.size - 1){
+//                    solution[outerNumberIndex] = -1
+//                }
+//            }
+//        }
+//
+////        println(solution.toList())
+//
+//        return solution
+//    }
+}
